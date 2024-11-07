cdffc7ea1a4bab2b1ff75fd2643456381ee32b3e
darren2581 <darrentanthongen@gmail.com>
2024-11-08 00:35:40 +0800

    First Part of Layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Hello Wolrd
+    <div className='container'>
+      {/* Search Section */}
+      <div className='search-section'>
+        <form action="" className='search-form'>
+        <span className="material-symbols-outlined">search</span>
+          <input type='search' className='search-input' placeholder='Enter a city name'/>
+        </form>
+        <button className='location-button'>
+        <span className="material-symbols-outlined">my_location</span>
+        </button>
+      </div>
+
+      {/* Weather Section */} 
+      <div className='weather-section'>
+        <div className='current-weather'>
+          <img src='icons/clouds.svg' className='weather-icon'/>
+          <h2 className='temperature'>
+            20 <span>°C</span>
+          </h2>
+          <p className='description'>Partly cloudy</p>
+        </div>
+
+        <div className='hourly-forecast'>
+          <ul className='weather-list'>
+            <li className='weather-item'>
+              <p className='time'>00:00</p>
+              <img src='icons/clouds.svg' className='weather-icon'/>
+              <p className='temperature'>20°C</p>
+            </li>
+            <li className='weather-item'>
+              <p className='time'>00:00</p>
+              <img src='icons/clouds.svg' className='weather-icon'/>
+              <p className='temperature'>20°C</p>
+            </li>
+            <li className='weather-item'>
+              <p className='time'>00:00</p>
+              <img src='icons/clouds.svg' className='weather-icon'/>
+              <p className='temperature'>20°C</p>
+            </li>
+            <li className='weather-item'>
+              <p className='time'>00:00</p>
+              <img src='icons/clouds.svg' className='weather-icon'/>
+              <p className='temperature'>20°C</p>
+            </li>
+            <li className='weather-item'>
+              <p className='time'>00:00</p>
+              <img src='icons/clouds.svg' className='weather-icon'/>
+              <p className='temperature'>20°C</p>
+            </li>
+            <li className='weather-item'>
+              <p className='time'>00:00</p>
+              <img src='icons/clouds.svg' className='weather-icon'/>
+              <p className='temperature'>20°C</p>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
